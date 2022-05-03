a830ecd117dcc97fcc85c1ff85ecc7be36b61826
patriciaturato <patricia.turato@aegro.com.br>
2022-05-02 22:23:12 -0300

    add methods to create a farm

--- farm/src/main/java/com/aegro/farm/entity/Farm.java
+++ farm/src/main/java/com/aegro/farm/entity/Farm.java
@@ -1,18 +1,31 @@
 package com.aegro.farm.entity;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Farm {
+    private String id;
     private String name;
     private List<Plot> plots;
     private BigDecimal productivity;
 
     public Farm() {
+        this.plots = new ArrayList<>();
     }
 
-    public Farm(String name) {
+    public Farm(String id, String name) {
+        this.id = id;
         this.name = name;
+        this.plots = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
---
+++ farm/src/main/java/com/aegro/farm/service/FarmService.java
@@ -0,0 +1,7 @@
+package com.aegro.farm.service;
+
+import com.aegro.farm.entity.Farm;
+
+public interface FarmService {
+    Farm create(Farm farm);
+}
